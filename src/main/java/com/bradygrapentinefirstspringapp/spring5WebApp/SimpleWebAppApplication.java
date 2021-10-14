0626177e104096b01861868ba8973834ba36095a
bradygrapentine <brady.grapentine@gmail.com>
2021-10-14 14:10:33 -0400

    added POJOs

--- src/main/java/com/bradygrapentinefirstspringapp/spring5WebApp/SimpleWebAppApplication.java
+++ src/main/java/com/bradygrapentinefirstspringapp/spring5WebApp/SimpleWebAppApplication.java
@@ -1,13 +1,12 @@
 package com.bradygrapentinefirstspringapp.spring5WebApp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleWebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleWebAppApplication.class, args);
+//		SpringApplication.run(SimpleWebAppApplication.class, args);
 	}
 
 }
