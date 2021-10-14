1f6fd9b6db2e2d81d0a7851e97614a7142d24e85
bradygrapentine <brady.grapentine@gmail.com>
2021-10-14 14:00:20 -0400

    initial commit

---
+++ src/test/java/com/bradygrapentinefirstspringapp/spring5WebApp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradygrapentinefirstspringapp.spring5WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
